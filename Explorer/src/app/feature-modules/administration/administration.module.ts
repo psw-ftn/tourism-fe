959ad9e2c38104f3179154bd5bde2117ab1c1df8
lukaDoric <doricluka12@gmail.com>
2023-09-24 14:54:28 +0200

    feat: interceptor added.

--- Explorer/src/app/feature-modules/administration/administration.module.ts
+++ Explorer/src/app/feature-modules/administration/administration.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EquipmentFormComponent } from './equipment-form/equipment-form.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { MaterialModule } from 'src/app/infrastructure/material/material.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EquipmentFormComponent,
+    EquipmentComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
+  ],
+  exports: [
+    EquipmentComponent,
+    EquipmentFormComponent
   ]
 })
 export class AdministrationModule { }
---
+++ Explorer/src/app/feature-modules/administration/equipment-form/equipment-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xp-equipment-form',
+  templateUrl: './equipment-form.component.html',
+  styleUrls: ['./equipment-form.component.css']
+})
+export class EquipmentFormComponent {
+
+}
